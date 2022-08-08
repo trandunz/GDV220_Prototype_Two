6a06a2e1019d30303dbbe568647077a55d0a02ea
OMaloso <88115296+OMaloso@users.noreply.github.com>
2022-08-09 09:53:03 +1200

    Added fade to black script and amended oxygen tank

--- Prototype_Two/Assets/StaceStuff/StaceScripts/OxygenTankValue.cs
+++ Prototype_Two/Assets/StaceStuff/StaceScripts/OxygenTankValue.cs
@@ -12,6 +12,14 @@ public class OxygenTankValue : MonoBehaviour
     public float fDrainSpeed4 = 0.0f;
     public float fDrainSpeed5 = 0.0f;
 
+    public GameObject fadeToBlack;
+
+    public bool bDamaged = false;
+    public float fDamageAmount = 0.5f;
+
+    public bool bDashed = false;
+    public float fDashAmount = 0.25f;
+
     Vector3 scaleAmount;
     [SerializeField] private int iMaxOxygen;
 
@@ -50,5 +58,50 @@ public class OxygenTankValue : MonoBehaviour
     void Update()
     {
         transform.localScale -= scaleAmount * Time.deltaTime;
+
+        // If oxygen is 0 (or less), then start fade to black script
+        if (transform.localScale.y <= 0.0f)
+        {
+            scaleAmount = new Vector3(0.0f, 0.0f, 0.0f);
+            fadeToBlack.GetComponent<FadeToBlack>().bFading = true;
+        }
+
+        // Losing oxygen when damaged
+        if (bDamaged == true)
+        {
+            if (transform.localScale.y > fDamageAmount)
+            {
+                transform.localScale -= new Vector3(0.0f, fDamageAmount, 0.0f);
+            }
+            else
+            {
+                transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            }
+            bDamaged = false;
+        }
+
+        if (bDashed == true)
+        {
+            if (transform.localScale.y > fDashAmount)
+            {
+                transform.localScale -= new Vector3(0.0f, fDashAmount, 0.0f);
+            }
+            else
+            {
+                transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            }
+            bDashed = false;
+        }
+
+        // Testing - forces fade to black
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            bDamaged = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            bDashed = true;
+        }
     }
 }
