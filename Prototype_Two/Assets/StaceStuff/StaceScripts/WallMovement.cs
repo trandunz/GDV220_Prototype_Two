3e323517570d9358f66ca1abfc343abced424e90
OMaloso <88115296+OMaloso@users.noreply.github.com>
2022-08-09 14:20:20 +1200

    Changed wall size so more of it can be seen. Now loops between 4 sets of walls instead of 4

--- Prototype_Two/Assets/StaceStuff/StaceScripts/WallMovement.cs
+++ Prototype_Two/Assets/StaceStuff/StaceScripts/WallMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class WallMovement : MonoBehaviour
 {
-    public float fWallHeight = 20.0f;
+    float fHeight = 20.0f;
 
     public GameObject Wall1;
     public GameObject Wall2;
+    public GameObject Wall3;
+    public GameObject Wall4;
 
     // Start is called before the first frame update
     void Start()
@@ -19,23 +21,43 @@ public class WallMovement : MonoBehaviour
     void Update()
     {
         // Wall 1 teleporting
-        if (transform.position.y <= (Wall1.transform.position.y - fWallHeight))
+        if (transform.position.y <= (Wall1.transform.position.y - (fHeight/2)))
         {
-            float fCurrentPositionY = Wall1.transform.position.y; // Get current position
+            //float fCurrentPositionY = Wall1.transform.position.y; // Get current position
 
-            float fNewPositionY = fCurrentPositionY - (fWallHeight * 2); // Calculate new position based on wall height
+            //float fNewPositionY = fCurrentPositionY - (fHeight * 2); // Calculate new position based on wall height
 
-            Wall1.transform.Translate(0.0f, -(fWallHeight * 2), 0.0f); // Move wall to new position
+            Wall1.transform.Translate(0.0f, -fHeight, 0.0f); // Move wall to new position
         }
 
         // Wall 2 teleporting
-        if (transform.position.y <= (Wall2.transform.position.y - fWallHeight))
+        if (transform.position.y <= (Wall2.transform.position.y - (fHeight/2)))
         {
-            float fCurrentPositionY = Wall2.transform.position.y;
+            //float fCurrentPositionY = Wall2.transform.position.y;
 
-            float fNewPositionY = fCurrentPositionY - (fWallHeight) * 2;
+            //float fNewPositionY = fCurrentPositionY - (fWallHeight) * 2;
 
-            Wall2.transform.Translate(0.0f, -(fWallHeight * 2), 0.0f);
+            Wall2.transform.Translate(0.0f, -fHeight, 0.0f);
+        }
+
+        // Wall 3 teleporting
+        if (transform.position.y <= (Wall3.transform.position.y - (fHeight / 2)))
+        {
+            //float fCurrentPositionY = Wall2.transform.position.y;
+
+            //float fNewPositionY = fCurrentPositionY - (fWallHeight) * 2;
+
+            Wall3.transform.Translate(0.0f, -fHeight, 0.0f);
+        }
+
+        // Wall 2 teleporting
+        if (transform.position.y <= (Wall4.transform.position.y - (fHeight / 2)))
+        {
+            //float fCurrentPositionY = Wall2.transform.position.y;
+
+            //float fNewPositionY = fCurrentPositionY - (fWallHeight) * 2;
+
+            Wall4.transform.Translate(0.0f, -fHeight, 0.0f);
         }
     }
 }
