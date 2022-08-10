b1ecf06047589cab77c4a75f8750f6194b6771c8
trandunz <54924605+trandunz@users.noreply.github.com>
2022-08-10 12:00:47 +1200

    + Possibly fixed crash

--- Prototype_Two/Assets/WillsStuff/Scripts/GemManager.cs
+++ Prototype_Two/Assets/WillsStuff/Scripts/GemManager.cs
@@ -39,11 +39,13 @@ public class GemManager : MonoBehaviour
     public void ResetGemCount()
     {
         PlayerPrefs.SetInt("Gems", 0);
+        PlayerPrefs.Save();
     }
 
     public void AddGems(int _amount)
     {
         PlayerPrefs.SetInt("Gems", PlayerPrefs.GetInt("Gems") + _amount);
+        PlayerPrefs.Save();
     }
 
     public void RemoveGems(int _amount)
@@ -52,5 +54,6 @@ public class GemManager : MonoBehaviour
         {
             PlayerPrefs.SetInt("Gems", PlayerPrefs.GetInt("Gems") - 1);
         }
+        PlayerPrefs.Save();
     }
 }
