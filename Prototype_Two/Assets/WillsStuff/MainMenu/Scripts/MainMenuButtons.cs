2866a6e7bd98afa9ebfbe711b86693caf0a120c8
trandunz <54924605+trandunz@users.noreply.github.com>
2022-08-11 15:25:27 +1200

    + main menu buttons lerping color

--- Prototype_Two/Assets/WillsStuff/MainMenu/Scripts/MainMenuButtons.cs
+++ Prototype_Two/Assets/WillsStuff/MainMenu/Scripts/MainMenuButtons.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class MainMenuButtons : MonoBehaviour
 {
     public GameObject audioSelect;
+    Color originalColor;
 
     public void StartGame()
     {
@@ -22,5 +23,55 @@ public class MainMenuButtons : MonoBehaviour
     public void ToggleMenu(GameObject _menu)
     {
         _menu.SetActive(!_menu.activeSelf);
+    }
+
+    public void HoverOverOption(TMPro.TextMeshProUGUI _text)
+    {
+        originalColor = _text.color;
+        Color color = new Color(255, 255, 255);
+        StartCoroutine(LerpColor(_text, color, 0.15f));
+    }
+
+    public void HoverOverOption(Image _text)
+    {
+        originalColor = _text.color;
+        Color color = new Color(255, 255, 255);
+        StartCoroutine(LerpColor(_text, color, 0.15f));
+    }
+
+    public void HoverLeftOption(TMPro.TextMeshProUGUI _text)
+    {
+        Debug.Log("Left Option");
+        Color color = originalColor;
+        StartCoroutine(LerpColor(_text, color, 0.15f));
+    }
+
+    public void HoverLeftOption(Image _text)
+    {
+        Debug.Log("Left Option");
+        Color color = originalColor;
+        StartCoroutine(LerpColor(_text, color, 0.15f));
+    }
+
+    IEnumerator LerpColor(TMPro.TextMeshProUGUI _text, Color _endColor, float _fadeTime)
+    {
+        float timeElapsed = 0f;
+        while (timeElapsed < _fadeTime)
+        {
+            _text.color = Color.Lerp(_text.color, _endColor, timeElapsed / _fadeTime);
+            timeElapsed += Time.unscaledDeltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator LerpColor(Image _text, Color _endColor, float _fadeTime)
+    {
+        float timeElapsed = 0f;
+        while (timeElapsed < _fadeTime)
+        {
+            _text.color = Color.Lerp(_text.color, _endColor, timeElapsed / _fadeTime);
+            timeElapsed += Time.unscaledDeltaTime;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
