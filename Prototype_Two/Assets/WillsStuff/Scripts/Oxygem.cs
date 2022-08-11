af7ac0650ed2d62ea8272bee400a837b9465e298
trandunz <54924605+trandunz@users.noreply.github.com>
2022-08-11 16:09:45 +1200

    + chest opening on sea floor

--- Prototype_Two/Assets/WillsStuff/Scripts/Oxygem.cs
+++ Prototype_Two/Assets/WillsStuff/Scripts/Oxygem.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Oxygem : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag is "Player")
-        {
-            GemManager.instance.AddGems(1);
-            Destroy(gameObject);
-        }
-    }
-
     private void Update()
     {
         transform.Rotate(new Vector3(0.0f, 40.0f * Time.deltaTime, 0.0f), Space.Self);
