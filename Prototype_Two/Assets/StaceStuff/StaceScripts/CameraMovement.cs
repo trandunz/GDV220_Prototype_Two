d61eda356f618068b5145093c59da15c71a0969e
OMaloso <88115296+OMaloso@users.noreply.github.com>
2022-10-01 16:51:53 +1300

    Added flares/glowsticks that slowly move downwards Background changes to darker shade of blue over time

--- Prototype_Two/Assets/StaceStuff/StaceScripts/CameraMovement.cs
+++ Prototype_Two/Assets/StaceStuff/StaceScripts/CameraMovement.cs
@@ -16,14 +16,29 @@ public class CameraMovement : MonoBehaviour
     public float timer = 0.0f;
     private float maxTimer;
 
+    // Background image - changing to darker blue over time
+    [Header("BackgroundColorChangeOverTime")]
+    public GameObject backgroundObject;
+    public Color backgroundColor;
+    public float colorG;
+    public float colorB;
+    public float timerBackground;
+    public float maxTimerBackground = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        timer = maxTimer;
-
         manager = GameObject.FindGameObjectWithTag("GameManager");
         darknessStartingDepth = manager.GetComponent<GameManager>().darknessStartingDepth;
         maxTimer = manager.GetComponent<GameManager>().darknessChangeSpeed;
+
+        // Screen lighting
+        timer = maxTimer;
+
+        // Background image 
+        colorG = backgroundObject.GetComponent<MeshRenderer>().material.color.g;
+        colorB = backgroundObject.GetComponent<MeshRenderer>().material.color.b;
+        timerBackground = maxTimerBackground;
     }
 
     // Update is called once per frame
@@ -42,7 +57,7 @@ public class CameraMovement : MonoBehaviour
         {
             timer = timer - 1.0f * Time.deltaTime; // Decrease timer by 1 second
 
-            if (timer <= 1.0f)
+            if (timer <= 0.0f)
             {
                 if (lightingLevel > 0)
                 {
@@ -51,6 +66,23 @@ public class CameraMovement : MonoBehaviour
                     timer = maxTimer;
                 }
             }
+        }
+
+        // Background image - make darker over time
+        timerBackground = timerBackground - 1.0f * Time.deltaTime; // Decrease timer by 1 second
+        if (timerBackground <= 0.0f)
+        {
+            // Change color of background mat
+            if (colorG >= 53.0f / 255.0f)
+            {
+                colorG -= 1.0f / 255.0f;
+            }
+            if (colorB >= 94.0f / 255.0f)
+            {
+                colorB -= 1.0f / 255.0f;
+            }
+            backgroundObject.GetComponent<MeshRenderer>().material.color = new Color(0, colorG, colorB);
+            timerBackground = maxTimerBackground; // reset timer
         }
     }
 }
