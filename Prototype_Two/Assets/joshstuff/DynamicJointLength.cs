7a0a3824182d3b8f6ae0fb44a3ae866190d3b315
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2022-10-05 10:28:31 +1300

    added joints to attempt to limit character movement

---
+++ Prototype_Two/Assets/joshstuff/DynamicJointLength.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicJointLength : MonoBehaviour
+{
+    [SerializeField] FastIKFabric player1;
+    [SerializeField] FastIKFabric player2;
+
+    [SerializeField] ConfigurableJoint player1Joint;
+    [SerializeField] ConfigurableJoint player2Joint;
+    [SerializeField] float reduction = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoftJointLimit limit1 = new SoftJointLimit();
+        limit1.limit = player1.CompleteLength;
+        player1Joint.linearLimit = limit1;
+
+        SoftJointLimit limit2 = new SoftJointLimit();
+        limit2.limit = player2.CompleteLength;
+        player2Joint.linearLimit = limit2;
+    }
+
+    private void FixedUpdate()
+    {
+        SoftJointLimit limit1 = new SoftJointLimit();
+        limit1.limit = player1.CompleteLength;
+        player1Joint.linearLimit = limit1;
+
+        SoftJointLimit limit2 = new SoftJointLimit();
+        limit2.limit = player2.CompleteLength;
+        player2Joint.linearLimit = limit2;
+    }
+}
