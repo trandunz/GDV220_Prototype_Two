a35d421e655db9066757da396da2be36c63c025e
trandunz <54924605+trandunz@users.noreply.github.com>
2022-10-13 19:33:34 +1300

    + angler wwith red eye

--- Prototype_Two/Assets/joshstuff/AnglerFish/AnglerFish.cs
+++ Prototype_Two/Assets/joshstuff/AnglerFish/AnglerFish.cs
@@ -9,15 +9,24 @@ public class AnglerFish : MonoBehaviour
     [SerializeField] float lifeTime = 5;
     [SerializeField] bool isMoving = false;
     private float knockBackForce = 500.0f;
+    [SerializeField] Animator m_Animator;
+    bool hasAttacked = false;
+    [SerializeField] Material m_RedAyeMaterial;
+    [SerializeField] SkinnedMeshRenderer m_Eye;
+    Material[] m_Materials;
 
     private void Start()
     {
+        m_Materials = m_Eye.materials;
     }
 
     private void FixedUpdate()
     {
         if (isMoving)
         {
+            m_Materials[1] = m_RedAyeMaterial;
+            m_Eye.materials = m_Materials;
+
             transform.parent.position = Vector3.MoveTowards(transform.parent.position, gem.transform.position, moveSpeed * Time.deltaTime);
             lifeTime -= Time.deltaTime;
 
@@ -30,7 +39,22 @@ public class AnglerFish : MonoBehaviour
 
     public void Attack()
     {
+        if (!hasAttacked)
+        {
+            hasAttacked = true;
+            StartCoroutine(ChompRoutine());
+        }
+        
+    }
+
+    IEnumerator ChompRoutine()
+    {
+        m_Animator.SetTrigger("Attack");
         isMoving = true;
+        float originalMoveSpeed = moveSpeed;
+        moveSpeed = 3.0f;
+        yield return new WaitForSeconds(0.4f);
+        moveSpeed = originalMoveSpeed;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -45,7 +69,8 @@ public class AnglerFish : MonoBehaviour
             Debug.Log(impulse.x);
             other.GetComponent<SwimController>().ApplyImpulse(impulse);
             other.GetComponent<SwimController>().HitEnemy();
-            Attack();
+            isMoving = true;
+            m_Animator.SetBool("Swim", true);
         }
     }
     private void OnTriggerStay(Collider other)
@@ -53,7 +78,7 @@ public class AnglerFish : MonoBehaviour
         if (other.tag is "Player")
         {
             other.GetComponent<SwimController>().StartInvulnrability();
-            Attack();
+            isMoving = true;
         }
     }
 }
