384ee275e7430832515e59db1f59293db008f1a5
trandunz <54924605+trandunz@users.noreply.github.com>
2022-10-18 16:23:38 +1300

    + fixed main menu

--- Prototype_Two/Assets/Art Assets/Environment/3D_BkGround/Materials/MMBuoyManager.cs
+++ Prototype_Two/Assets/Art Assets/Environment/3D_BkGround/Materials/MMBuoyManager.cs
@@ -9,65 +9,85 @@ public class MMBuoyManager : MonoBehaviour
     public GameObject audioSplash;
     [SerializeField] GameObject PauseMenu;
     bool m_inPauseMenu = false;
+    bool m_inHighScore = true;
+    bool canDoStuff = false;
+    [SerializeField] float m_StartDelay = 0.5f;
 
     private void Start()
     {
         PauseMenu.gameObject.SetActive(false);
+        
     }
     private void Update()
     {
-        if (!m_inPauseMenu)
+        if (m_StartDelay > 0)
         {
-            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                Destroy(Instantiate(audioSplash), 3.0f);
-                m_CurrentSelelection++;
-                if (m_CurrentSelelection >= m_Buoys.Length)
-                {
-                    m_CurrentSelelection = 0;
-                }
-                Debug.Log(m_CurrentSelelection);
-            }
-            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                Destroy(Instantiate(audioSplash), 3.0f);
-                m_CurrentSelelection--;
-                if (m_CurrentSelelection < 0)
-                {
-                    m_CurrentSelelection = m_Buoys.Length - 1;
-                }
-                Debug.Log(m_CurrentSelelection);
-            }
-
-            if (Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp(KeyCode.Backspace))
-            {   
-                Debug.Log("Option Chosen = " + m_CurrentSelelection);
-
-                if (m_CurrentSelelection == 0)
-                    LevelLoader.instance.LoadLevel(1);
-                else if (m_CurrentSelelection == 1)
-                    PauseMenu.SetActive(true);
-                else if (m_CurrentSelelection == 2)
-                    LevelLoader.instance.LoadLevel(1);
-                else if (m_CurrentSelelection == 3)
-                    Application.Quit();
-            }
-
-            for (int i = 0; i < m_Buoys.Length; i++)
-            {
-                if (i != m_CurrentSelelection)
-                    m_Buoys[i].SetSelected(false);
-                else
-                    m_Buoys[i].SetSelected(true);
-            }
+            m_StartDelay -= Time.deltaTime;
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (!canDoStuff)
             {
-                PauseMenu.SetActive(false);
+                Destroy(Instantiate(audioSplash), 3.0f);
+                canDoStuff = true;
+            }
+            
+            if (!m_inPauseMenu && !m_inHighScore)
+            {
+                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    Destroy(Instantiate(audioSplash), 3.0f);
+                    m_CurrentSelelection++;
+                    if (m_CurrentSelelection >= m_Buoys.Length)
+                    {
+                        m_CurrentSelelection = 0;
+                    }
+                    Debug.Log(m_CurrentSelelection);
+                }
+                if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    Destroy(Instantiate(audioSplash), 3.0f);
+                    m_CurrentSelelection--;
+                    if (m_CurrentSelelection < 0)
+                    {
+                        m_CurrentSelelection = m_Buoys.Length - 1;
+                    }
+                    Debug.Log(m_CurrentSelelection);
+                }
+
+                if (Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp(KeyCode.Backspace))
+                {
+                    Debug.Log("Option Chosen = " + m_CurrentSelelection);
+
+                    if (m_CurrentSelelection == 0)
+                        LevelLoader.instance.LoadLevel(1);
+                    else if (m_CurrentSelelection == 1)
+                        PauseMenu.SetActive(true);
+                    else if (m_CurrentSelelection == 2)
+                        LevelLoader.instance.LoadLevel(1);
+                    else if (m_CurrentSelelection == 3)
+                        Application.Quit();
+                }
+
+                for (int i = 0; i < m_Buoys.Length; i++)
+                {
+                    if (i != m_CurrentSelelection)
+                        m_Buoys[i].SetSelected(false);
+                    else
+                        m_Buoys[i].SetSelected(true);
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    PauseMenu.SetActive(false);
+                }
             }
         }
+        
+
+        m_inHighScore = FindObjectOfType<EnterHighScore>();
 
         m_inPauseMenu = PauseMenu.activeSelf;
     }
